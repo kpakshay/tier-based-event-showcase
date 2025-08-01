0318303905f015c7bcc941a5a4866a7033567260
Akshay <kpakshay98@gmail.com>
2025-08-02 03:28:01 +0530

    Events displayed using cards

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,7 +1,72 @@
-const Event=()=>{
-    return(
+"use client"
+import { useState, useEffect } from "react"
+
+const Event = () => {
+
+    const [events, setEvents] = useState([])
+    useEffect(() => {
+        async function fetchEvents() {
+            const result = await fetch('api/event')
+            const data = await result.json()
+            console.log(data)
+            setEvents(data)
+        }
+        fetchEvents()
+        console.log(events)
+    }, [])
+
+    return (
         <>
-            <h2>Events For you</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 px-4 justify-items-center p-6">
+                {events.map((item) => {
+                    const formattedDate = new Date(item.event_date).toLocaleDateString('en-GB');
+                    return (
+                        <div className="w-full max-w-md bg-white dark:bg-gray-800 rounded-lg p-6 shadow-md">
+                            <div>
+                                <span className="inline-flex items-center justify-center rounded-md bg-gray-400 p-1 shadow-lg">
+                                    {/* <img src={`${item.image_url}`} alt="Image"></img> */}
+                                    <img src={`${item.image_url}`} alt="Event Banner" className="w-100 h-80 object-cover rounded-md"/>
+                                </span>
+                            </div>
+                            <h3 className="text-gray-900 dark:text-white mt-4 text-lg font-semibold">
+                                {item.title}
+                            </h3>
+                            <p className="text-gray-500 dark:text-gray-400 mt-2 text-sm">{item.description}</p>
+                            <p className="text-gray-400 dark:text-gray-300 mt-1 text-sm">📅 {formattedDate}</p>
+                            <span
+                                className={`capitalize inline-block mt-2 px-3 py-1 text-xs font-semibold rounded-full 
+                                    ${item.tier === 'platinum'
+                                        ? 'bg-gradient-to-r from-gray-300 to-gray-100 text-black shadow shadow-cyan-500/50'
+                                        : item.tier === 'gold'
+                                            ? 'bg-yellow-400 text-white shadow'
+                                            : item.tier === 'silver'
+                                                ? 'bg-gray-400 text-white'
+                                                : item.tier === 'free'
+                                                    ? 'bg-green-500 text-white'
+                                                    : 'bg-gray-500 text-white'
+                                    }`}
+                            >
+                                {item.tier}
+                            </span>
+                        </div>
+                    );
+                })}
+            </div>
+
+            {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center p-3">
+                <div class="bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
+                    <div>
+                        <span class="inline-flex items-center justify-center rounded-md bg-indigo-500 p-2 shadow-lg">
+                            <svg class="h-6 w-6 stroke-white">
+                            </svg>
+                        </span>
+                    </div>
+                    <h3 class="text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight ">Writes upside-down</h3>
+                    <p class="text-gray-500 dark:text-gray-400 mt-2 text-sm ">
+                        The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
+                    </p>
+                </div>
+            </div> */}
         </>
     )
 }
--- src/app/navbar/page.js
+++ src/app/navbar/page.js
@@ -27,23 +27,14 @@ const NavBar = () => {
                         </div>
                         <div class="hidden sm:ml-6 sm:block">
                             <div class="flex space-x-4">
-                                {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
                                 <a href="/home"  class={`rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white ${pathname=="/home"?"bg-gray-700 text-white":''}`}>Dashboard</a>
                                 <a href="/events" class={`rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white ${pathname=="/events"?"bg-gray-700 text-white":''}`}>Events</a>
-                                {/* <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a> */}
                                 <a href="/aboutus" class={`rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white ${pathname=="/aboutus"?"bg-gray-700 text-white":''}`}>About Us</a>
                             </div>
                         </div>
                     </div>
                     <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                            <span class="absolute -inset-1.5"></span>
-                            <span class="sr-only">View notifications</span>
-                            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6">
-                                <path d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" stroke-linecap="round" stroke-linejoin="round" />
-                            </svg>
-                        </button>
-
+                        
                         {/* <!-- Profile dropdown --> */}
                         <el-dropdown class="relative ml-3">
                             <button class="relative flex rounded-full bg-gray-800 text-sm focus:outline-hidden focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-offset-2 focus-visible:ring-offset-gray-800">
